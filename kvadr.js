d51dff5e80974513e9dacc62bebdc1259444ac2a
grialalaku <skyjackerz@icloud.com>
2020-04-15 18:31:46 +0900

    js01

--- kvadr.js
+++ kvadr.js
@@ -1,26 +1,26 @@
-// напишите функции x1 и x2 и измените функцию solve, 
-// так, чтобы она показывала решения (решение) 
-// уравнения, введенного пользователем на странице 
-
 function solve() {
-	// получаем данные в виде строк из полей ввода на странице
+
+	// напишите функции x1 и x2 и измените функцию solve, 
+	// так, чтобы она показывала решения (решение) 
+	// уравнения, введенного пользователем на странице 	
+
 	var a = document.getElementById("a").value;
 	var b = document.getElementById("b").value;
 	var c = document.getElementById("c").value;
 	// получаем числа из строк
 	a = parseFloat(a);
 	b = parseFloat(b);
-	c = parseFloat(c);	
+	c = parseFloat(c);
 	// дальше вам надо заменить код, чтобы он показывал решения,
 	// или строку "РЕШЕНИЙ НЕТ", если дискриминант уравнения < 0
-	alert(D(a,b,c));
-}
-
-function D(a,b,c){
-	// вычисление дискриминанта по формуле
-	return b*b - 4*a*c;
-}
-
-function x1(a,b,c,D){
-	// вычисление меньшего корня
+	var D = b*b - 4*a*c;
+	var x1 = (b+(Math.sqrt(D)))/(2*a);
+	var x2 = (b-(Math.sqrt(D)))/(2*a);
+	if (D<0) {
+		alert("Нет решения");
+	} else {
+		document.getElementById('D').value = D;
+		document.getElementById('x1').value = x1;
+		document.getElementById('x2').value = x2;
+	}
 }
